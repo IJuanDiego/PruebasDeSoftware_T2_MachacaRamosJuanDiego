0fa8fb1ce137598c96bc8bd427325105ec296863
IJuanDiego <i202336416@cibertec.edu.pe>
2026-04-13 21:08:00 -0500

    Pregunta 2

--- src/test/java/edu/pe/cibertec/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/edu/pe/cibertec/stepdefinitions/LoginStepDefinitions.java
@@ -1,4 +1,59 @@
 package edu.pe.cibertec.stepdefinitions;
 
+import edu.pe.cibertec.tasks.IniciarSesionComo;
+import edu.pe.cibertec.ui.LoginPage;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Open;
+import org.openqa.selenium.WebDriver;
+import static org.hamcrest.Matchers.containsString;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+
 public class LoginStepDefinitions {
+
+    @Managed
+    private WebDriver browser;
+
+    private Actor juan;
+
+    @Before
+    public void setUp(){
+        juan = Actor.named("juan").whoCan(BrowseTheWeb.with(browser));
+        browser.manage().deleteAllCookies();
+    }
+
+    @Given("the user navigates to the login page")
+    public void the_user_navigates_to_the_login_page() {
+        juan.attemptsTo(Open.url(LoginPage.URL));
+    }
+
+    @When("the user enters email {string} and password {string}")
+    public void the_user_enters_email_and_password(String email, String password) {
+        juan.attemptsTo(IniciarSesionComo.withCredentials(email, password));
+    }
+
+    @And("clicks on the login button")
+    public void clicks_on_the_login_button() {
+        juan.attemptsTo(Click.on(LoginPage.LOGIN_BUTTON));
+    }
+
+    @Then("the user should see the name {string} in the page header")
+    public void the_user_should_see_the_name_in_the_page_header(String name) {
+        juan.should(seeThat("Nombre en header",
+                        actor -> LoginPage.HEADER_NAME.resolveFor(actor).getText(),
+                        containsString("My Account")));
+    }
+
+    @Then("the user should see the error message {string}")
+    public void the_user_should_see_the_error_message(String message) {
+        juan.should(seeThat("Error message", actor -> LoginPage.ERROR_MESSAGE.resolveFor(actor).getText(), containsString(message)));
+    }
+
 }
--- src/test/java/edu/pe/cibertec/ui/LoginPage.java
+++ src/test/java/edu/pe/cibertec/ui/LoginPage.java
@@ -1,4 +1,21 @@
 package edu.pe.cibertec.ui;
 
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
 public class LoginPage {
+
+    public static final String URL = "https://naveenautomationlabs.com/opencart/index.php?route=account/login";
+
+    public static final Target EMAIL_FIELD = Target.the("email field").located(By.id("input-email"));
+
+    public static final Target PASSWORD_FIELD = Target.the("password field").located(By.name("password"));
+
+    public static final Target LOGIN_BUTTON = Target.the("login button").located(By.cssSelector("input[type='submit']"));
+
+    public static final Target HEADER_NAME = Target.the("user name in header").located(By.cssSelector("#content h2, .col-sm-9 h2"));
+
+    public static final Target ERROR_MESSAGE = Target.the("error message").located(By.cssSelector(".alert-danger"));
 }
+
+
